2e50fac2cbc5053a65a08a382ef25d8a67fc326c
Alex Friedrichsen <afriedrichsen@me.com>
2017-09-16 20:30:41 -0500

    Updated MongoDB information

--- public/js/googleAnalytics.js
+++ public/js/googleAnalytics.js
@@ -1,10 +1,6 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-35736272-3']);
-_gaq.push(['_setDomainName', 'willmayner.com']);
-_gaq.push(['_trackPageview']);
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'UA-73090776-2');
