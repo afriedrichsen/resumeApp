838eef52c6b53ce6ab34d1f42c44d8484909f029
Alex Friedrichsen <afriedrichsen@me.com>
2017-09-30 23:15:05 -0500

    Added updated GA tag.

--- public/js/googleAnalytics.js
+++ public/js/googleAnalytics.js
@@ -1,6 +1,5 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments)};
+gtag('js', new Date());
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)};
-  gtag('js', new Date());
-
-  gtag('config', 'UA-73090776-2');
+gtag('config', 'UA-73090776-2');
