ca8214a37f7500fcff8b699c2da8c59660820058
Alex Friedrichsen <afriedrichsen@me.com>
2017-10-01 00:04:46 -0500

    Updated JADE templates.

--- app.js
+++ app.js
@@ -48,9 +48,9 @@ http.createServer(app).listen(app.get('port'), function() {
 //http.createServer(app).listen(443);
 
 //GA
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments)};
-gtag('js', new Date());
+//window.dataLayer = window.dataLayer || [];
+//function gtag(){dataLayer.push(arguments)};
+//gtag('js', new Date());
 
-gtag('config', 'UA-73090776-2');
+//gtag('config', 'UA-73090776-2');
 
