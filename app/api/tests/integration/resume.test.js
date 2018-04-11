06b24c2364cfc81d0a4ba9e11aa92b377c29eb85
Alex Friedrichsen <afriedrichsen@me.com>
2018-04-11 00:56:17 -0500

    Added test case for index route.

---
+++ app/api/tests/integration/resume.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const httpStatus = require('http-status');
+//const { expect } = require('chai');
+//const sinon = require('sinon');
+//const bcrypt = require('bcryptjs');
+//const { some, omitBy, isNil } = require('lodash');
+const app = require('../../../index');
+
+
+describe ('Resume API', async () => {
+    it('should get resume index page', () => {
+        return request(app)
+            .get('/')
+            .expect(httpStatus.OK)
+            .then(async (res, err) => {
+              if (err) {
+                  return err;
+              }
+        })
+    });
+});
