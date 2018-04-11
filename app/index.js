48dd1dee1282fc921554d1266dbc173cf2eb658e
Alex Friedrichsen <afriedrichsen@me.com>
2018-04-11 01:00:02 -0500

    Cleaned up indentation for chai.

--- app/index.js
+++ app/index.js
@@ -1,13 +1,17 @@
 // make bluebird default Promise
+
 Promise = require('bluebird'); // eslint-disable-line no-global-assign
 const { port, env } = require('./api/config/vars');
 const app = require('./api/config/express');
 
-//const mongoose = require('./config/mongoose');
+// const mongoose = require('./config/mongoose');
 
 // open mongoose connection
-//mongoose.connect();
+
+// mongoose.connect();
+
 console.log(process.env.NODE_ENV);
+
 // listen to requests
 app.listen(port, () => console.info(`server started on port ${port} (${env})`));
 
@@ -15,4 +19,5 @@ app.listen(port, () => console.info(`server started on port ${port} (${env})`));
 * Exports express
 * @public
 */
+
 module.exports = app;
