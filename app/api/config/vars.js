32a6c92d88c041f1c3b702798f11a55389fa7661
Alex Friedrichsen <afriedrichsen@me.com>
2018-04-29 14:45:31 -0500

    Added latest mongoose configuration.

--- app/api/config/vars.js
+++ app/api/config/vars.js
@@ -12,11 +12,11 @@ module.exports = {
     jwtSecret: process.env.JWT_SECRET,
     jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
 
-    // mongo: {
-   //     uri: process.env.NODE_ENV === 'test'
-   //         ? process.env.MONGO_URI_TESTS
-   //         : process.env.MONGO_URI,
-   // },
+     mongo: {
+        uri: process.env.NODE_ENV === 'test'
+            ? process.env.MONGO_URI_TESTS
+            : process.env.MONGO_URI,
+    },
 
     logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
 };
