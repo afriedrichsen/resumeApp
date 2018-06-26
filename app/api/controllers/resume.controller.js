0de61de7413048a7459ac1983fc8e06f17874167
Alex Friedrichsen <afriedrichsen@me.com>
2018-06-26 13:15:19 -0500

    Fixed test case.

--- app/api/controllers/resume.controller.js
+++ app/api/controllers/resume.controller.js
@@ -11,9 +11,9 @@ exports.index = (req, res, next) => {
   try {
 
    Resume.find({user_name: 'Alexander Friedrichsen'}).exec().then((resumedata) => {
-       const result = JSON.parse(JSON.stringify(resumedata[0]));
+       const result = JSON.parse(JSON.stringify({data: resumedata[0]}));
        // Pass resume to view engine (pug template).
-       res.render('index',result);
+       res.render('index',result.data);
   });
 
   } catch (exception) {
